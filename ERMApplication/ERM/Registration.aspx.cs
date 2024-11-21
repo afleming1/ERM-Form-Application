c6c4c300214ec1a9b4835e0d7bf01b4eecc3c246
MichiganKnight <drew.p.fleming@gmail.com>
2024-11-20 23:57:22 -0600

    Add Default Landing Page

--- ERMApplication/ERM/Registration.aspx.cs
+++ ERMApplication/ERM/Registration.aspx.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Configuration;
 using System.Data.SqlClient;
-using System.Diagnostics;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace ERMApplication.ERM
 {
