6a96b95e4aa5a9b1219142df7b43b74d18c875e9
MichiganKnight <drew.p.fleming@gmail.com>
2024-11-20 23:43:09 -0600

    Add Database Access

---
+++ ERMApplication/ERM/Error.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/ERM/Site.Master" AutoEventWireup="true" CodeBehind="Error.aspx.cs" Inherits="ERMApplication.ERM.Error" %>
+
+<asp:Content ID="Content" ContentPlaceHolderID="ContentPlaceHolder" runat="server">
+    <h2>Error</h2>
+
+    <p>Something bad happened. We are attempting to fix it...</p>
+</asp:Content>
--- ERMApplication/ERM/Registration.aspx.cs
+++ ERMApplication/ERM/Registration.aspx.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -18,7 +21,7 @@ namespace ERMApplication.ERM
         {
             if (textFirstName.Text == string.Empty)
             {
-                ErrorPanel.Visible = false;
+                ErrorPanel.Visible = true;
                 ErrorMessage.Text = "Please provide your first name";
 
                 textFirstName.Focus();
@@ -27,7 +30,7 @@ namespace ERMApplication.ERM
             }
             else if (textLastName.Text == string.Empty)
             {
-                ErrorPanel.Visible = false;
+                ErrorPanel.Visible = true;
                 ErrorMessage.Text = "Please provide your last first name";
 
                 textLastName.Focus();
@@ -36,7 +39,7 @@ namespace ERMApplication.ERM
             }
             else if (textEmail.Text == string.Empty)
             {
-                ErrorPanel.Visible = false;
+                ErrorPanel.Visible = true;
                 ErrorMessage.Text = "Please provide your email address";
 
                 textEmail.Focus();
@@ -45,16 +48,16 @@ namespace ERMApplication.ERM
             }
             else if (textConfirmEmail.Text == string.Empty)
             {
-                ErrorPanel.Visible = false;
+                ErrorPanel.Visible = true;
                 ErrorMessage.Text = "Please confirm your email address";
 
                 textConfirmEmail.Focus();
 
                 return false;
             }
-            else if (textEmail != textConfirmEmail)
+            else if (textEmail.Text != textConfirmEmail.Text)
             {
-                ErrorPanel.Visible = false;
+                ErrorPanel.Visible = true;
                 ErrorMessage.Text = "Please double check and confirm your email address";
 
                 textConfirmEmail.Focus();
@@ -63,7 +66,7 @@ namespace ERMApplication.ERM
             }
             else if (textAddress.Text == string.Empty)
             {
-                ErrorPanel.Visible = false;
+                ErrorPanel.Visible = true;
                 ErrorMessage.Text = "Please provide your address";
 
                 textAddress.Focus();
@@ -72,7 +75,7 @@ namespace ERMApplication.ERM
             }
             else if (textCity.Text == string.Empty)
             {
-                ErrorPanel.Visible = false;
+                ErrorPanel.Visible = true;
                 ErrorMessage.Text = "Please provide your city";
 
                 textCity.Focus();
@@ -81,7 +84,7 @@ namespace ERMApplication.ERM
             }
             else if (textState.Text == string.Empty)
             {
-                ErrorPanel.Visible = false;
+                ErrorPanel.Visible = true;
                 ErrorMessage.Text = "Please provide your state";
 
                 textState.Focus();
@@ -90,7 +93,7 @@ namespace ERMApplication.ERM
             }
             else if (listRates.SelectedValue == "NULL")
             {
-                ErrorPanel.Visible = false;
+                ErrorPanel.Visible = true;
                 ErrorMessage.Text = "Please select a rate";
 
                 listRates.Focus();
@@ -99,7 +102,7 @@ namespace ERMApplication.ERM
             }
             else if (optionRegular.Checked == false && optionKosher.Checked == false && optionVegetarian.Checked == false && optionVegan.Checked == false && optionFruit.Checked == false && optionGluten.Checked == false && optionLactose.Checked == false)
             {
-                ErrorPanel.Visible = false;
+                ErrorPanel.Visible = true;
                 ErrorMessage.Text = "Please choose a lunch option";
 
                 optionRegular.Focus();
@@ -112,11 +115,90 @@ namespace ERMApplication.ERM
             }
         }
 
+        protected bool AddRegistration()
+        {
+            try
+            {
+                string connectionString = ConfigurationManager.ConnectionStrings["ERMConnection"].ConnectionString;
+                string SQL = "INSERT INTO RegistrationTable (RegID, FirstName, LastName, Email, Address, City, State, Rates, Lunch, Audio, Visual, Mobile, DateTimeCreated) VALUES (@RegID, @FirstName, @LastName, @EmailAddress, @Rate, @Lunch, @Audio, @Visual, @Mobile, @DateTimeCreated)";
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    SqlCommand command = new SqlCommand(SQL, connection);
+
+                    command.Parameters.AddWithValue("@RegID", Guid.NewGuid().ToString());
+
+                    command.Parameters.AddWithValue("@FirstName", textFirstName.Text);
+                    command.Parameters.AddWithValue("@FirstName", textLastName.Text);
+
+                    command.Parameters.AddWithValue("@EmailAddress", textEmail.Text);
+
+                    command.Parameters.AddWithValue("@Address", textAddress.Text);
+                    command.Parameters.AddWithValue("@City", textCity.Text);
+                    command.Parameters.AddWithValue("@State", textState.Text);
+
+                    command.Parameters.AddWithValue("@Rate", listRates.SelectedValue);
+
+                    if (optionRegular.Checked == true)
+                    {
+                        command.Parameters.AddWithValue(@"Lunch", "Regular");
+                    }
+                    else if (optionKosher.Checked == true)
+                    {
+                        command.Parameters.AddWithValue(@"Lunch", "Kosher");
+                    }
+                    else if (optionVegetarian.Checked == true)
+                    {
+                        command.Parameters.AddWithValue(@"Lunch", "Vegetarian");
+                    }
+                    else if (optionVegan.Checked == true)
+                    {
+                        command.Parameters.AddWithValue(@"Lunch", "Vegan");
+                    }
+                    else if (optionFruit.Checked == true)
+                    {
+                        command.Parameters.AddWithValue(@"Lunch", "Fruit Plate");
+                    }
+                    else if (optionGluten.Checked == true)
+                    {
+                        command.Parameters.AddWithValue(@"Lunch", "Gluten Free");
+                    }
+                    else if (optionLactose.Checked == true)
+                    {
+                        command.Parameters.AddWithValue(@"Lunch", "Lactose Free");
+                    }
+
+                    command.Parameters.AddWithValue("@Audio", checkAudio.Checked);
+                    command.Parameters.AddWithValue("@Visual", CheckVisual.Checked);
+                    command.Parameters.AddWithValue("@Mobile", CheckMobile.Checked);
+
+                    command.Parameters.AddWithValue("@DateTimeCreated", DateTime.Now);
+
+                    connection.Open();
+
+                    command.ExecuteNonQuery();
+                }
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
         protected void buttonSubmit_Click(object sender, EventArgs e)
         {
             if (VerifyForm() == true)
             {
-                Response.Redirect("ThankYou.aspx");
+                if (AddRegistration() == true)
+                {
+                    Response.Redirect("ThankYou.aspx");
+                }
+                else
+                {
+                    Response.Redirect("Error.aspx");
+                }
             }
         }        
 
